b505591de0943cbfb0e34172aa7867cb1b0bd552
Siva369IT <sivasatyamsetti369@gmail.com>
2025-03-16 17:21:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -206,13 +206,7 @@ if uploaded_files:
 
 # ✅ Copyright (Fixed at bottom)
 st.markdown(
-    """
-    <div style="position: fixed; bottom: 0; width: 100%; text-align: center; padding: 10px; background-color: white;">
-        <p style="font-size: 12px; color: #555;">
-            © Content Owners: <b>Pavan Sri Sai Mondem</b> | <b>Siva Satyamsetti</b> | <b>Uma Satyam Mounika Sapireddy</b> |
-            <b>Bhuvaneswari Devi Seru</b> | <b>Chandu Meela</b> | Trainees from <b>Techwing</b> 🧡
-        </p>
-    </div>
-    """,
-    unsafe_allow_html=True
+'<p class="small-text">© Content Owners: Pavan Sri Sai Mondem | Siva Satyamsetti | Uma Satyam Mounika Sapireddy | '
+'Bhuvaneswari Devi Seru | Chandu Meela | Trainees from techwing 🧡</p>',
+unsafe_allow_html=True
 )
