ee52baa25f37f924869d0e6a6386837b07d4eb16
Siva369IT <sivasatyamsetti369@gmail.com>
2025-03-23 11:15:21 +0530

    Update app.py v-21

--- app.py
+++ app.py
@@ -9,9 +9,31 @@ import fitz  # PyMuPDF
 from docx import Document
 from pptx import Presentation
 import zipfile
+import base64
 
 st.set_page_config(page_title="PDF & File Converter", layout="centered")
 st.image("logo1.png", use_container_width=True)
+import base64
+
+def set_background(png_file):
+    with open(png_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{encoded_string.decode()}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+set_background("logo1.png")
 st.title("PDF & File Converter App")
 
 operation = st.selectbox(
