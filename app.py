01c805b630d2426e6966d63e3e5034b9bde6a1c9
Siva369IT <sivasatyamsetti369@gmail.com>
2025-03-23 13:45:14 +0530

    Update app.py v-21

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ st.title("PDF & File Converter App")
 operation = st.selectbox(
     "Choose an Operation",
     (
-        "Click here to choose 👇"
+        "Click here to choose 👇",
         "Generate Empty PDF",
         "Convert Any File to PDF",
         "Extract Pages from PDF",
